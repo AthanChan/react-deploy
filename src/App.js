d7ccca90e6661de54cd4061955578c62216f62fb
Athan Chan <athan.chan@berkeley.edu>
2022-01-09 18:21:15 -0800

    css file and general format

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
   return (
     <div className="App">
-      <h1> My Athan React Website </h1>
-      <h1> My Athan React Website </h1>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <Navbar />
+      <div className='content'>
+        <Home />
+      </div>
     </div>
   );
 }
+
 
 export default App;
